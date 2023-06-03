d848068508a20524d18125916f24487294710845
Zireael112 <86360076+Zireael112@users.noreply.github.com>
2023-06-03 17:34:16 +0300

    Update createPopover.test.js

--- src/js/__tests__/createPopover.test.js
+++ src/js/__tests__/createPopover.test.js
@@ -1,22 +1,13 @@
+/* eslint-disable jest/valid-expect */
 import createPopover from "../createPopover";
 import "jest";
 
 test("should return true", () => {
-  window.addEventListener(
-    ("DOMContentLoaded",
-    () => {
-      expect(createPopover()).toBe(true);
-    })
-  );
+  expect(createPopover()).toBe(true);
 });
 
 test("should return false", () => {
-  window.addEventListener(
-    ("DOMContentLoaded",
-    () => {
-      const popover = document.createElement("div");
-      popover.classList.add("popover");
-      expect(createPopover()).toBe(false);
-    })
-  );
+  const popover = document.createElement("div");
+  popover.classList.add("popover");
+  expect(createPopover()).toBe(false);
 });
